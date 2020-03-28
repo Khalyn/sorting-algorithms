96f78ad7a9be6bcdd234759cb603a5a202ab7142
Melvyn <melvyn.piau@epitech.eu>
2020-03-28 15:56:40 -0400

    [SelectionSort]: Add first version of selection sort algorithm

---
+++ include/SortingAlgorithms.hpp
@@ -0,0 +1,14 @@
+/*
+** sorting-algorithms
+*/
+
+#ifndef PROJET_SORTINGALGORITHMS_HPP
+#define PROJET_SORTINGALGORITHMS_HPP
+
+#include "vector"
+
+namespace sort {
+	void selection(std::vector<int> &array);
+}
+
+#endif //PROJET_SORTINGALGORITHMS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 /*
 ** sorting-algorithms
-** File description:
-** Created by Khalyn
 */
 
+#include <iostream>
+#include "SortingAlgorithms.hpp"
+
+void	displayVector(const std::vector<int> &array) {
+	std::cout << "Vector: ";
+	for (const auto &elem : array) {
+		std::cout << elem << ' ';
+	}
+	std::cout << std::endl;
+}
+
 int	main() {
+	std::vector<int> array = {64, 25, 22, 12, 11};
+	displayVector(array);
+	sort::selection(array);
+	displayVector(array);
 	return 0;
 }
